501248c0f69d54db6d554888692dcbfbad4c1b04
tbsimmen <marc.simmen@hslu.ch>
2014-12-10 16:55:13 +0100

    .gitignore is now working

--- FRDM_/Debug/Generated_Code/heap_3.d
+++
@@ -1,4 +0,0 @@
-Generated_Code/heap_3.o: ../Generated_Code/heap_3.c \
- ../Generated_Code/FreeRTOSConfig.h
-
-../Generated_Code/FreeRTOSConfig.h:
--- FRDM_/Debug/Generated_Code/heap_4.d
+++
@@ -1,4 +0,0 @@
-Generated_Code/heap_4.o: ../Generated_Code/heap_4.c \
- ../Generated_Code/FreeRTOSConfig.h
-
-../Generated_Code/FreeRTOSConfig.h:
